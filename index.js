49513aeb0ace2a5a6ef840cd3de464da8320f567
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-05-11 20:34:22 -0300

    Nueva función para guardar la info en localStorage

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let inputTarea = document.querySelector("#inputTarea");
 let formTareas = document.querySelector("#formTareas");
 let listTareas = document.querySelector("#listTareas");
 
+if (localStorage.getItem("tareas")) {
+  listTareas.innerHTML = localStorage.getItem("tareas");
+}
+
 formTareas.addEventListener("submit", function (event) {
   event.preventDefault();
   const tarea = inputTarea.value;
@@ -10,18 +14,24 @@ formTareas.addEventListener("submit", function (event) {
     nuevaTarea.innerText = tarea;
     listTareas.appendChild(nuevaTarea);
 
-    const fechaTexto = document.createElement("span");
-    fechaTexto.innerText = new Date().toLocaleString();
-    nuevaTarea.appendChild(fechaTexto);
-
     const eliminarTarea = document.createElement("button");
-    eliminarTarea.innerText = "X";
+    eliminarTarea.innerText = "Eliminar";
     nuevaTarea.appendChild(eliminarTarea);
 
     eliminarTarea.onclick = function () {
       nuevaTarea.remove();
     };
+
+    const fechaTexto = document.createElement("span");
+    fechaTexto.innerText = new Date().toLocaleString();
+    nuevaTarea.appendChild(fechaTexto);
   }
 
   inputTarea.value = "";
+
+  localStorage.setItem("tareas", listTareas.innerHTML);
+});
+
+window.addEventListener("beforeunload", function () {
+  localStorage.setItem("tareas", listTareas.innerHTML);
 });
